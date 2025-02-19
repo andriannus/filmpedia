0cf81f615bf0d50fca854f1e5781915796e520b6
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-02-19 15:28:52 +0700

    feat: download font and adjust provider for React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,41 @@
-import Providers from './providers';
+import localFont from 'next/font/local';
+
+import ReactQueryProvider from './provider';
+
 import './main.scss';
+
+const montserrat = localFont({
+  src: [
+    {
+      path: '../public/fonts/montserrat-v29-latin-regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/montserrat-v29-latin-500.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/montserrat-v29-latin-600.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/montserrat-v29-latin-700.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
-        <Providers>{children}</Providers>
+      <body className={`${montserrat.className}`}>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
